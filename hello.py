b066fcd9f47629fe8e2228aaf6d6cb2a5302082d
David Murhi <davidmurhhi@gmail.com>
2026-04-21 04:35:59 +1000

    Week 1: variables, data types, user input

---
+++ hello.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("What city are you in? ")
+
+print("Nice to meet you,", name)
+print("You are", age, "years old and you live in", city)
